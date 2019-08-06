55b5508ba861825d86d6721f764bc4c016e17280
Vyas Ramankulangara <vyas01890@outlook.com>
2019-08-06 01:12:30 -0500

    reordered server.js routes

--- server.js
+++ server.js
@@ -9,11 +9,11 @@ const PORT = process.env.PORT || 4000;
 app.get('/', (req, res) => res.send('Home Page'));
 
 app.use('/api/admin', require('./routes/api/admin'));
-// app.use('/api/event', require('./routes/api/event'));
-app.use('/api/member', require('./routes/api/member'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/officers', require('./routes/api/officer'));
 app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/member', require('./routes/api/member'));
+app.use('/api/officers', require('./routes/api/officer'));
+app.use('/api/profile', require('./routes/api/profile'));
+// app.use('/api/event', require('./routes/api/event'));
 app.use('/api/resume', require('./routes/api/resume'));
 
 app.listen(PORT, () => console.log(`Listening to port ${PORT}`));
