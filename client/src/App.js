e9112c01bd33b80b9bd2df4218f12248f8baf695
Jacob Medel <jmedel@uh.edu>
2019-08-26 19:36:20 -0500

    Router pages setup

--- client/src/App.js
+++ client/src/App.js
@@ -2,13 +2,23 @@ import React from 'react';
 import './App.css';
 import NavBar from './components/Navbar/Navbar';
 // import bodyImage from '../assets/'
+import { BrowserRouter as Router, Route} from "react-router-dom";
+import Events from './Events';
+import About from './About';
+import Membership from './Membership';
+import Home from './Home';
+import Careerfair from './Careerfair';
 
 function App() {
   return (
-    <div>
-      <NavBar></NavBar>
-      
-    </div>
+    <Router>
+      <NavBar/>
+        <Route path="/" exact component={Home} />
+        <Route path="/about/" component={About} />
+        <Route path="/membership/" component={Membership} />
+        <Route path="/events/" component={Events} />
+        <Route path="/careerfair/" component={Careerfair} />
+    </Router>
   );
 }
 
---
+++ client/src/Events.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+class Events extends React.Component {
+    render() {
+        return (
+            <div>Events</div>
+        );
+    }
+}
+export default Events;
