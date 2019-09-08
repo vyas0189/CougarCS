1c30ed573b723a1108201ad61fb3650a406353c3
Vyas Ramankulangara <vyas01890@outlook.com>
2019-09-07 21:55:26 -0500

    updated Events

--- client/src/pages/Events/Events.js
+++ client/src/pages/Events/Events.js
@@ -1,10 +1,20 @@
+import dayGridPlugin from '@fullcalendar/daygrid';
+import FullCalendar from '@fullcalendar/react';
 import React from 'react';
-
+import { Container } from 'react-bootstrap';
+import events from '../../events';
+import './Event.css';
 class Events extends React.Component {
-    render() {
-        return (
-            <div>Events</div>
-        );
-    }
+  render() {
+    return (
+      <Container>
+        <FullCalendar
+          defaultView="dayGridMonth"
+          plugins={[dayGridPlugin]}
+          events={events}
+        />
+      </Container>
+    );
+  }
 }
 export default Events;
