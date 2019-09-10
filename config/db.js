7f99c0371dea76649d0084b1ba19b83ed7eba478
Vyas Ramankulangara <vyas01890@outlook.com>
2019-09-10 08:53:59 -0500

    test

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 const config = require('config');
 
-const db = process.env.MONGO_URI;
+const db = process.env.mongo_uri;
 // config.get('MONGO_URI');
 
 const connectDB = async () => {
